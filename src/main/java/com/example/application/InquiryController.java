285e5f8d11a08445888b272298a271943cb6c1fc
takaki miura <takaki.miura@broadleaf.co.jp>
2022-07-13 14:11:11 +0900

    inquiryフォームとconfirmのページを作成(未完成:名前の結果が2つ出てくる,空っぽでもエラーが出ない)

---
+++ src/main/java/com/example/application/InquiryController.java
@@ -0,0 +1,45 @@
+package com.example.application;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/inquiry")
+public class InquiryController {
+
+  @GetMapping("/form")
+  public String form(InquiryForm inquiryForm, Model m) {
+    m.addAttribute("title", "Inquiry Form");
+    
+    return "/form";
+  }
+
+  @PostMapping("/form")
+  public String formGoBack(InquiryForm inquiryForm, Model m) {
+    m.addAttribute("title", "Inquiry Form");
+    
+    return "/form";
+  }
+
+  @PostMapping("/confirm")
+  public String confirm(@Validated InquiryForm inquiryForm,
+  BindingResult result,
+  Model m
+  ) {
+    if(result.hasErrors()) {
+      m.addAttribute("title", "Inquiry Form");
+
+      return "/form";
+    }
+    m.addAttribute("title", "Confirm Page");
+
+    return "/confirm";
+
+  }
+  
+}
