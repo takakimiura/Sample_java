1e1cd17bd5fda84f293eb5602c5d6917c0c91229
takaki miura <takaki.miura@broadleaf.co.jp>
2022-07-13 16:58:28 +0900

    sueveyformの表示までを完了

--- src/main/java/com/example/application/SurveyController.java
+++ src/main/java/com/example/application/SurveyController.java
@@ -1,5 +1,24 @@
 package com.example.application;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+@RequestMapping("/survey")
 public class SurveyController {
   
+  @GetMapping("/form")
+  public String form(SurveyForm surveyForm,
+  Model m,
+  @ModelAttribute("complete") String complete
+  ) {
+    m.addAttribute("title", "Survey Form");
+
+    return "/exform";
+  }
 }
--- src/main/java/com/example/application/SurveyForm.java
+++ src/main/java/com/example/application/SurveyForm.java
@@ -1,5 +1,46 @@
 package com.example.application;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
+
 public class SurveyForm {
   
+  @Min(0) @Max(150)
+  private int age;
+
+  private int satisfaction;
+
+  @Size(min = 1, max = 200, message = "1~200文字までで入力してください")
+  private String comment;
+
+  public SurveyForm() {
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public int getSatisfaction() {
+    return satisfaction;
+  }
+
+  public void setSatisfaction(int satisfaction) {
+    this.satisfaction = satisfaction;
+  }
+
+  public String getComment() {
+    return comment;
+  }
+
+  public void setComment(String comment) {
+    this.comment = comment;
+  }
+
+  
+
 }
