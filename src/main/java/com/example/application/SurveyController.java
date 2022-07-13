01a5a8cde35c8e3266837101082c7be38de966c1
takaki miura <takaki.miura@broadleaf.co.jp>
2022-07-13 17:11:04 +0900

    フォームから確認ページまでの処理を完了

--- src/main/java/com/example/application/SurveyController.java
+++ src/main/java/com/example/application/SurveyController.java
@@ -2,9 +2,12 @@ package com.example.application;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -20,5 +23,30 @@ public class SurveyController {
     m.addAttribute("title", "Survey Form");
 
     return "/exform";
+  }
+
+  @PostMapping("/form")
+  public String formBack(SurveyForm surveyForm, Model m) {
+
+    m.addAttribute("title", "Survey Form");
+
+    return "/exform";
+  }
+
+  @PostMapping("/confirm")
+  public String confirm(@Validated SurveyForm surveyForm,
+  BindingResult result,
+  Model m
+  ) {
+
+    if(result.hasErrors()) {
+      m.addAttribute("title", "Survey Form");
+
+      return "/exform";
+    }
+
+    m.addAttribute("title", "Confirm Page");
+
+    return "/exconfirm";
   }
 }
